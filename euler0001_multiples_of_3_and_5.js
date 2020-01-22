9c0388c22be56a22dc665082f7b76478ed2c6261
Guido Canella <guidoc@sermig.org>
2020-01-22 18:18:13 +0100

    Update euler0001_multiples_of_3_and_5.js

--- euler0001_multiples_of_3_and_5.js
+++ euler0001_multiples_of_3_and_5.js
@@ -1,6 +1,6 @@
 function multiplesOf3and5(number) {
-  var x3 = Math.floor((number-1)/3);
-  var x5 = Math.floor((number-1)/5);
-  var x15 = Math.floor((number-1)/15);
+  let x3 = Math.floor((number-1)/3);
+  let x5 = Math.floor((number-1)/5);
+  let x15 = Math.floor((number-1)/15);
   return 3*x3*(x3+1)/2 + 5*x5*(x5+1)/2 - 15*x15*(x15+1)/2;
 }
