b0aacffd8e16684ee5f7a573a24927dfac59c2c8
admin <admin@HP-G62>
2020-02-01 16:57:48 +0100

    Problems 10, 11.

--- problems/euler0010_summation_of_primes.js
+++ problems/euler0010_summation_of_primes.js
@@ -1,5 +1,13 @@
 function primeSummation(n) {
-	return primesBelowN(n).reduce((tot,curr)=>{return tot+curr;});
+	let primes = [2,3,5], curr, j, sum=10;
+	for (curr=7; curr<n; curr+=2) {
+		for (j=1; Math.pow(primes[j],2) <= curr && curr % primes[j]; j++) {}
+		if (Math.pow(primes[j],2) > curr) {
+			primes.push(curr);
+			sum += curr;
+		}
+	}
+	return sum;
 }
 
 function main(n) { return primeSummation(n);}
