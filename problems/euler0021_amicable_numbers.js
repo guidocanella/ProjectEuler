c64fba9ac6eb9a85d4f4dff002c6366d92f294a8
admin <admin@HP-G62>
2020-02-17 21:32:56 +0100

    Problem 23.

--- problems/euler0021_amicable_numbers.js
+++ problems/euler0021_amicable_numbers.js
@@ -1,14 +1,7 @@
 function sumAmicableNum(n) {
   let sum=0, dd=[];
   function d(m) {
-    if (typeof dd[m] === 'undefined') {
-      dd[m]=1;
-      let i;
-      for (i=2; i<=Math.sqrt(m); i++) {
-        if (m%i==0) dd[m]+=i+m/i;
-      }
-      if (i==Math.sqrt(m)) dd[m]-=i;
-    }
+    if (typeof dd[m] === 'undefined') dd[m] = properDivisorsSum(m);
     return dd[m];
   }
   for (let j=1; j<n; j++) {
